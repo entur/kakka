1837f0964271e87ce6642fe87dc14dfa58989e96
erlendnils1 <erlendnils1@gmail.com>
2018-08-14 13:57:02 +0200

    Attempt to create polygon from list of coordinates before calculating centroid. Hopefully this will yield a better centroid for the typical case.

--- src/main/java/no/entur/kakka/geocoder/netex/pbf/TopographicPlaceOsmContentHandler.java
+++ src/main/java/no/entur/kakka/geocoder/netex/pbf/TopographicPlaceOsmContentHandler.java
@@ -29,13 +29,28 @@ import org.opentripplanner.openstreetmap.model.OSMRelation;
 import org.opentripplanner.openstreetmap.model.OSMWay;
 import org.opentripplanner.openstreetmap.model.OSMWithTags;
 import org.opentripplanner.openstreetmap.services.OpenStreetMapContentHandler;
-import org.rutebanken.netex.model.*;
+import org.rutebanken.netex.model.CountryRef;
+import org.rutebanken.netex.model.IanaCountryTldEnumeration;
+import org.rutebanken.netex.model.KeyListStructure;
+import org.rutebanken.netex.model.KeyValueStructure;
+import org.rutebanken.netex.model.LocationStructure;
+import org.rutebanken.netex.model.ModificationEnumeration;
+import org.rutebanken.netex.model.MultilingualString;
+import org.rutebanken.netex.model.SimplePoint_VersionStructure;
+import org.rutebanken.netex.model.TopographicPlace;
+import org.rutebanken.netex.model.TopographicPlaceDescriptor_VersionedChildStructure;
+import org.rutebanken.netex.model.TopographicPlaceTypeEnumeration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.util.CollectionUtils;
 
 import java.math.BigDecimal;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.BlockingQueue;
 import java.util.stream.Collectors;
 
@@ -46,168 +61,181 @@ import java.util.stream.Collectors;
  * first to collect nodes ids referred by relevant ways and then to map relevant nodes and ways.
  */
 public class TopographicPlaceOsmContentHandler implements OpenStreetMapContentHandler {
-	private static final Logger logger = LoggerFactory.getLogger(TopographicPlaceNetexWriter.class);
+    private static final Logger logger = LoggerFactory.getLogger(TopographicPlaceNetexWriter.class);
 
-	private BlockingQueue<TopographicPlace> topographicPlaceQueue;
+    private BlockingQueue<TopographicPlace> topographicPlaceQueue;
 
-	private List<String> tagFilters;
+    private List<String> tagFilters;
 
-	private String participantRef;
+    private String participantRef;
 
-	private IanaCountryTldEnumeration countryRef;
+    private IanaCountryTldEnumeration countryRef;
 
-	private static final String TAG_NAME = "name";
+    private static final String TAG_NAME = "name";
 
-	private Map<Long, OSMNode> nodes = new HashMap<>();
+    private Map<Long, OSMNode> nodes = new HashMap<>();
 
-	private Set<Long> nodeRefsUsedInWays = new HashSet<>();
+    private Set<Long> nodeRefsUsedInWays = new HashSet<>();
 
-	private boolean gatherNodesUsedInWaysPhase = true;
+    private boolean gatherNodesUsedInWaysPhase = true;
 
-	public TopographicPlaceOsmContentHandler(BlockingQueue<TopographicPlace> topographicPlaceQueue,
-			                                        List<String> tagFilters, String participantRef, IanaCountryTldEnumeration countryRef) {
-		this.topographicPlaceQueue = topographicPlaceQueue;
-		this.tagFilters = cleanFilter(tagFilters);
-		this.participantRef = participantRef;
-		this.countryRef = countryRef;
-	}
+    public TopographicPlaceOsmContentHandler(BlockingQueue<TopographicPlace> topographicPlaceQueue,
+                                                    List<String> tagFilters, String participantRef, IanaCountryTldEnumeration countryRef) {
+        this.topographicPlaceQueue = topographicPlaceQueue;
+        this.tagFilters = cleanFilter(tagFilters);
+        this.participantRef = participantRef;
+        this.countryRef = countryRef;
+    }
 
-	private List<String> cleanFilter(List<String> rawFilter) {
-		if (CollectionUtils.isEmpty(rawFilter)) {
-			return new ArrayList<>();
-		}
-		return rawFilter.stream().filter(f -> !StringUtils.isEmpty(f)).map(String::trim).collect(Collectors.toList());
-	}
+    private List<String> cleanFilter(List<String> rawFilter) {
+        if (CollectionUtils.isEmpty(rawFilter)) {
+            return new ArrayList<>();
+        }
+        return rawFilter.stream().filter(f -> !StringUtils.isEmpty(f)).map(String::trim).collect(Collectors.toList());
+    }
 
-	@Override
-	public void addNode(OSMNode osmNode) {
-		if (matchesFilter(osmNode)) {
-			TopographicPlace topographicPlace = map(osmNode).withCentroid(toCentroid(osmNode.lat, osmNode.lon));
-			topographicPlaceQueue.add(topographicPlace);
-		}
+    @Override
+    public void addNode(OSMNode osmNode) {
+        if (matchesFilter(osmNode)) {
+            TopographicPlace topographicPlace = map(osmNode).withCentroid(toCentroid(osmNode.lat, osmNode.lon));
+            topographicPlaceQueue.add(topographicPlace);
+        }
 
-		if (nodeRefsUsedInWays.contains(osmNode.getId())) {
-			nodes.put(osmNode.getId(), osmNode);
-		}
-	}
+        if (nodeRefsUsedInWays.contains(osmNode.getId())) {
+            nodes.put(osmNode.getId(), osmNode);
+        }
+    }
 
-	@Override
-	public void addWay(OSMWay osmWay) {
-		if (matchesFilter(osmWay)) {
+    @Override
+    public void addWay(OSMWay osmWay) {
+        if (matchesFilter(osmWay)) {
 
-			if (gatherNodesUsedInWaysPhase) {
-				nodeRefsUsedInWays.addAll(osmWay.getNodeRefs());
-			} else {
-				TopographicPlace topographicPlace = map(osmWay);
-				if (addGeometry(osmWay, topographicPlace)) {
-					topographicPlaceQueue.add(topographicPlace);
-				}
-			}
-		}
-	}
+            if (gatherNodesUsedInWaysPhase) {
+                nodeRefsUsedInWays.addAll(osmWay.getNodeRefs());
+            } else {
+                TopographicPlace topographicPlace = map(osmWay);
+                if (addGeometry(osmWay, topographicPlace)) {
+                    topographicPlaceQueue.add(topographicPlace);
+                }
+            }
+        }
+    }
 
-	private boolean addGeometry(OSMWay osmWay, TopographicPlace topographicPlace) {
-		List<Coordinate> coordinates = new ArrayList<>();
-		for (Long nodeRef : osmWay.getNodeRefs()) {
-			OSMNode node = nodes.get(nodeRef);
-			if (node != null) {
-				coordinates.add(new Coordinate(node.lon, node.lat));
-			}
-		}
+    private boolean addGeometry(OSMWay osmWay, TopographicPlace topographicPlace) {
+        List<Coordinate> coordinates = new ArrayList<>();
+        for (Long nodeRef : osmWay.getNodeRefs()) {
+            OSMNode node = nodes.get(nodeRef);
+            if (node != null) {
+                coordinates.add(new Coordinate(node.lon, node.lat));
+            }
+        }
 
-		if (coordinates.size() != osmWay.getNodeRefs().size()) {
-			logger.info("Ignoring osmWay with missing nodes: " + osmWay.getAssumedName());
-			return false;
-		}
-		topographicPlace.withCentroid(toCentroid(coordinates));
-		try {
-			topographicPlace.withPolygon(toPolygon(coordinates, participantRef + "-" + osmWay.getId()));
-		} catch (RuntimeException e) {
-			logger.info("Could not create polygon for osm way: " + osmWay.getAssumedName() + ". Exception: " + e.getMessage());
-		}
-		return true;
-	}
+        if (coordinates.size() != osmWay.getNodeRefs().size()) {
+            logger.info("Ignoring osmWay with missing nodes: " + osmWay.getAssumedName());
+            return false;
+        }
+        topographicPlace.withCentroid(toCentroid(coordinates));
+        try {
+            topographicPlace.withPolygon(toPolygon(coordinates, participantRef + "-" + osmWay.getId()));
+        } catch (RuntimeException e) {
+            logger.info("Could not create polygon for osm way: " + osmWay.getAssumedName() + ". Exception: " + e.getMessage());
+        }
+        return true;
+    }
 
-	@Override
-	public void doneSecondPhaseWays() {
-		gatherNodesUsedInWaysPhase = false;
-	}
+    @Override
+    public void doneSecondPhaseWays() {
+        gatherNodesUsedInWaysPhase = false;
+    }
 
-	boolean matchesFilter(OSMWithTags entity) {
-		if (!entity.hasTag(TAG_NAME)) {
-			return false;
-		}
+    boolean matchesFilter(OSMWithTags entity) {
+        if (!entity.hasTag(TAG_NAME)) {
+            return false;
+        }
 
-		for (Map.Entry<String, String> tag : entity.getTags().entrySet()) {
-			if (tagFilters.stream().anyMatch(f -> (tag.getKey() + "=" + tag.getValue()).startsWith(f))) {
-				return true;
-			}
-		}
-		return false;
-	}
+        for (Map.Entry<String, String> tag : entity.getTags().entrySet()) {
+            if (tagFilters.stream().anyMatch(f -> (tag.getKey() + "=" + tag.getValue()).startsWith(f))) {
+                return true;
+            }
+        }
+        return false;
+    }
 
-	TopographicPlace map(OSMWithTags entity) {
-		return new TopographicPlace()
-				       .withVersion("any")
-				       .withModification(ModificationEnumeration.NEW)
-				       .withName(multilingualString(entity.getAssumedName().toString()))
-				       .withDescriptor(new TopographicPlaceDescriptor_VersionedChildStructure().withName(multilingualString(entity.getAssumedName().toString())))
-				       .withTopographicPlaceType(TopographicPlaceTypeEnumeration.PLACE_OF_INTEREST)
-				       .withCountryRef(new CountryRef().withRef(countryRef))
-				       .withId(prefix(entity.getId()))
-				       .withKeyList(new KeyListStructure().withKeyValue(mapKeyValues(entity)));
-	}
+    TopographicPlace map(OSMWithTags entity) {
+        return new TopographicPlace()
+                       .withVersion("any")
+                       .withModification(ModificationEnumeration.NEW)
+                       .withName(multilingualString(entity.getAssumedName().toString()))
+                       .withDescriptor(new TopographicPlaceDescriptor_VersionedChildStructure().withName(multilingualString(entity.getAssumedName().toString())))
+                       .withTopographicPlaceType(TopographicPlaceTypeEnumeration.PLACE_OF_INTEREST)
+                       .withCountryRef(new CountryRef().withRef(countryRef))
+                       .withId(prefix(entity.getId()))
+                       .withKeyList(new KeyListStructure().withKeyValue(mapKeyValues(entity)));
+    }
 
-	List<KeyValueStructure> mapKeyValues(OSMWithTags entity) {
-		return entity.getTags().entrySet().stream()
-				       .filter(e -> !TAG_NAME.equals(e.getKey()))
-				       .map(e -> new KeyValueStructure().withKey(e.getKey()).withValue(e.getValue()))
-				       .collect(Collectors.toList());
-	}
+    List<KeyValueStructure> mapKeyValues(OSMWithTags entity) {
+        return entity.getTags().entrySet().stream()
+                       .filter(e -> !TAG_NAME.equals(e.getKey()))
+                       .map(e -> new KeyValueStructure().withKey(e.getKey()).withValue(e.getValue()))
+                       .collect(Collectors.toList());
+    }
 
-	protected String prefix(long id) {
-		return participantRef + ":TopographicPlace:" + id;
-	}
-
-
-	protected MultilingualString multilingualString(String val) {
-		return new MultilingualString().withLang("no").withValue(val);
-	}
+    protected String prefix(long id) {
+        return participantRef + ":TopographicPlace:" + id;
+    }
 
 
-	private PolygonType toPolygon(List<Coordinate> coordinates, String id) {
-		Polygon polygon = new GeometryFactory().createPolygon(coordinates.toArray(new Coordinate[coordinates.size()]));
-		return NetexGeoUtil.toNetexPolygon(polygon).withId(id);
-	}
+    protected MultilingualString multilingualString(String val) {
+        return new MultilingualString().withLang("no").withValue(val);
+    }
 
 
-	SimplePoint_VersionStructure toCentroid(List<Coordinate> coordinates) {
-		Point centroid = new GeometryFactory().createMultiPoint(coordinates.toArray(new Coordinate[coordinates.size()])).getCentroid();
-		return toCentroid(centroid.getY(), centroid.getX());
-	}
+    private PolygonType toPolygon(List<Coordinate> coordinates, String id) {
+        Polygon polygon = new GeometryFactory().createPolygon(coordinates.toArray(new Coordinate[coordinates.size()]));
+        return NetexGeoUtil.toNetexPolygon(polygon).withId(id);
+    }
 
 
-	SimplePoint_VersionStructure toCentroid(double latitude, double longitude) {
-		return new SimplePoint_VersionStructure().withLocation(
-				new LocationStructure().withLatitude(new BigDecimal(latitude))
-						.withLongitude(new BigDecimal(longitude)));
-	}
+    /**
+     * Calculate centroid of list of coordinates.
+     * <p>
+     * If coordinates may be converted to a polygon, the polygons centroid is used. If not, the centroid of the corresponding multipoint is used.
+     *
+     * @param coordinates
+     * @return
+     */
+    SimplePoint_VersionStructure toCentroid(List<Coordinate> coordinates) {
+        Point centroid;
+        try {
+            centroid = new GeometryFactory().createPolygon(coordinates.toArray(new Coordinate[coordinates.size()])).getCentroid();
+        } catch (RuntimeException re) {
+            centroid = new GeometryFactory().createMultiPoint(coordinates.toArray(new Coordinate[coordinates.size()])).getCentroid();
+        }
+        return toCentroid(centroid.getY(), centroid.getX());
+    }
 
-	@Override
-	public void addRelation(OSMRelation osmRelation) {
-		// Ignore
-	}
 
-	@Override
-	public void doneFirstPhaseRelations() {
-		// Ignore
-	}
+    SimplePoint_VersionStructure toCentroid(double latitude, double longitude) {
+        return new SimplePoint_VersionStructure().withLocation(
+                new LocationStructure().withLatitude(new BigDecimal(latitude))
+                        .withLongitude(new BigDecimal(longitude)));
+    }
+
+    @Override
+    public void addRelation(OSMRelation osmRelation) {
+        // Ignore
+    }
+
+    @Override
+    public void doneFirstPhaseRelations() {
+        // Ignore
+    }
 
 
-	@Override
-	public void doneThirdPhaseNodes() {
-		// Ignore
-	}
+    @Override
+    public void doneThirdPhaseNodes() {
+        // Ignore
+    }
 
 
 }

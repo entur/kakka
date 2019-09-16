e3c12f72a975326368114a8960afa0e0401f0563
vincent <vincent.paturet@entur.org>
2019-09-16 14:58:49 +0200

    ROR-647 Migrating JobEventQueue to Google PubSub

--- src/test/java/no/entur/kakka/repository/UniqueDigestPerFilenameIdempotentRepositoryTest.java
+++ src/test/java/no/entur/kakka/repository/UniqueDigestPerFilenameIdempotentRepositoryTest.java
@@ -16,21 +16,16 @@
 
 package no.entur.kakka.repository;
 
+import no.entur.kakka.KakkaRouteBuilderIntegrationTestBase;
 import no.entur.kakka.config.IdempotentRepositoryConfig;
 import no.entur.kakka.domain.FileNameAndDigest;
 import org.junit.Assert;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,classes = IdempotentRepositoryConfig.class, properties = "spring.main.sources=no.entur.kakka.test")
-@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
-public class UniqueDigestPerFilenameIdempotentRepositoryTest {
-
+public class UniqueDigestPerFilenameIdempotentRepositoryTest extends KakkaRouteBuilderIntegrationTestBase {
 
 	@Autowired
 	private UniqueDigestPerFileNameIdempotentRepository idempotentRepository;

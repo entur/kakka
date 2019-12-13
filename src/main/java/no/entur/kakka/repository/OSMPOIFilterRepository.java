c954ace32fc215dfbcf816c8cf44f0fabe86f157
Tom Erik Støwer <testower@gmail.com>
2019-12-13 10:31:13 +0100

    Drop redundant sql query

--- src/main/java/no/entur/kakka/repository/OSMPOIFilterRepository.java
+++ src/main/java/no/entur/kakka/repository/OSMPOIFilterRepository.java
@@ -8,7 +8,5 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface OSMPOIFilterRepository extends JpaRepository<OSMPOIFilter, Long> {
-
-    @Query("select f from OSMPOIFilter f WHERE f.key LIKE :key AND f.value LIKE :value")
     List<OSMPOIFilter> getByKeyAndValue(@Param("key") String key, @Param("value") String value);
 }

d787084b8458e769e09a9d38edd4c0e54411841f
Assad Riaz <assad.riaz@entur.org>
2020-11-10 10:59:11 +0100

    WTK-163 Implement OSM download route in kakka

--- src/main/java/no/entur/kakka/Constants.java
+++ src/main/java/no/entur/kakka/Constants.java
@@ -50,5 +50,8 @@ public class Constants {
 
     public static final String ET_CLIENT_NAME_HEADER = "ET-Client-Name";
     public static final String ET_CLIENT_ID_HEADER = "ET-Client-ID";
+
+    public static final String FILE_TARGET_MD5 = "RutebankenMd5SumRecordedForTargetFile" ;
+    public static final String CAMEL_ALL_HTTP_HEADERS = "CamelHttp*" ;
 }
 

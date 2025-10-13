e96d11d3e28821f736bfe5699bd134fb5be4a10e
Assad Riaz <assad.riaz@entur.org>
2025-10-13 08:53:54 +0200

    Update Google terraform provider

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 4.26"
+      version = "~> 6.49.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"

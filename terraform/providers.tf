4503df3d047c9714a723fe2f5f170351e0ddb958
assadriaz <assad.riaz@entur.org>
2025-12-09 15:15:32 +0100

    Remove Kubernetes provider from providers.tf
    Removed Kubernetes provider configuration.

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -6,9 +6,5 @@ terraform {
       source  = "hashicorp/google"
       version = "~> 6.49.0"
     }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "~> 2.13.1"
-    }
   }
 }
